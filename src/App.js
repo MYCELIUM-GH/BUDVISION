09728a68113f19d6f5213fcbc31e1d5b06910cb2
MYCELIUM-GH <108955160+MYCELIUM-GH@users.noreply.github.com>
2023-03-26 00:59:27 +0200

    5 files modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./style.css";
+import Header from "./component/Header";
 import Page from "./component/Page";
+import styled from "styled-components";
 export default function App() {
   return (
     <Page>
-    <div>
+      <Header
+        name=": : : : NETTECH_BANK : : : :"
+      />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
-    </div>
+    
     </Page>
   );
 }
---
+++ src/component/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Header({name}) {
+  return (
+    <HeaderTag>
+      <LogoTag>{name}</LogoTag>
+    </HeaderTag>
+  );
+}
+const HeaderTag = styled.div`
+  background: #27271b;
+  color: #d0ed57;
+  border-bottom: 2px solid;
+  border-bottom-left-radius: 20px;
+  border-bottom-right-radius: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+`;
+const LogoTag = styled.div`
+  color: #d0ed57;
+  font-weight: 450;
+  text-shadow: #d0ed57 -1px -2px 0.7em;
+`;
