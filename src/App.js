902a741dee3e36813c310a1916a9a446c79d11ca
MYCELIUM-GH <108955160+MYCELIUM-GH@users.noreply.github.com>
2023-03-26 01:14:45 +0200

    2 files modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from "react";
-import "./style.css";
-import Header from "./component/Header";
-import Page from "./component/Page";
-import styled from "styled-components";
+import React from 'react';
+import '../public/style.css';
+import Header from './component/Header';
+import Page from './component/Page';
+import styled from 'styled-components';
 export default function App() {
   return (
     <Page>
-      <Header
-        name="BUDVISION"
-      />
+      <Header name="BUDVISION" />
     </Page>
   );
 }
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import "../public/style.css";
 
 export default function Header({ name }) {
   return (
