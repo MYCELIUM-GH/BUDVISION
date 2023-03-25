c490c3cd34b437e19528a959197fc122fb07ae73
MYCELIUM-GH <108955160+MYCELIUM-GH@users.noreply.github.com>
2023-03-25 23:24:23 +0200

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./style.css";
-
+import Page from "./component/Page";
 export default function App() {
   return (
+    <Page>
     <div>
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
+    </Page>
   );
 }
---
+++ src/component/Page.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default function Page({ children }) {
+  return <PageTag>{children}</PageTag>;
+}
+
+const PageTag = styled.div`
+  background: linear-gradient(0deg, #121117 19.68%, #160f17 89.55%);
+  width: 100%;
+`;
